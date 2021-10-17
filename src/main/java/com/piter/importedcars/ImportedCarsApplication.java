8a70b31af9667e802f70097a45e46c128cdc62b6
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2021-10-17 14:24:27 +0200

    init

---
+++ src/main/java/com/piter/importedcars/ImportedCarsApplication.java
@@ -0,0 +1,13 @@
+package com.piter.importedcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportedCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportedCarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piter/importedcars/ImportedCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piter.importedcars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportedCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
