20d1ac488094643f008d7a69e5363034f1b1c026
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2023-04-02 19:33:22 +0200

    [FEATURE] Camel routes tests

--- src/test/java/com/piter/importedcars/route/ErrorHandlingRouteTest.java
+++ src/test/java/com/piter/importedcars/route/ErrorHandlingRouteTest.java
@@ -1,5 +1,8 @@
 package com.piter.importedcars.route;
 
+import static com.piter.importedcars.util.JsonResourceUtil.convertToJson;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.piter.importedcars.exception.CarReportException;
 import com.piter.importedcars.exception.ErrorResponse;
 import com.piter.importedcars.util.ExchangeUtils;
@@ -22,20 +25,21 @@ class ErrorHandlingRouteTest {
   private MockEndpoint mockEndpoint;
 
   @Test
-  public void shouldHandleError() throws InterruptedException {
+  public void shouldHandleError() throws InterruptedException, JsonProcessingException {
     //given
     var message = "Car report contains error";
     var exception = new CarReportException(message);
     var exchange = ExchangeUtils.createExchangeWithException(exception);
-    var expectedErrorResponse = new ErrorResponse(CarReportException.class.getSimpleName(), message);
+    var errorResponse = new ErrorResponse(CarReportException.class.getSimpleName(), message);
+    var expectedErrorJson = convertToJson(errorResponse);
+
+    mockEndpoint.expectedMessageCount(1);
+    mockEndpoint.expectedBodiesReceived(expectedErrorJson);
 
     //when
     producerTemplate.send(ErrorHandlingRoute.ERROR_DIRECT_URI, exchange);
 
     //then
     mockEndpoint.assertIsSatisfied();
-    mockEndpoint.message(0)
-        .body(ErrorResponse.class)
-        .isEqualTo(expectedErrorResponse);
   }
 }
