1a16788a00e17deb387701f73111859e68583459
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2023-04-02 13:56:27 +0200

    [FEATURE] Refactoring

--- src/main/java/com/piter/importedcars/validation/SearchFromDateValidator.java
+++ src/main/java/com/piter/importedcars/validation/SearchFromDateValidator.java
@@ -6,8 +6,10 @@ import javax.validation.ConstraintValidatorContext;
 
 public class SearchFromDateValidator implements ConstraintValidator<SearchFromDateConstraint, LocalDate> {
 
+  private static final int MAX_SEARCH_YEARS_IN_HISTORY = 2;
+
   @Override
   public boolean isValid(LocalDate localDate, ConstraintValidatorContext cxt) {
-    return localDate.isAfter(LocalDate.now().minusYears(2));
+    return localDate.isAfter(LocalDate.now().minusYears(MAX_SEARCH_YEARS_IN_HISTORY));
   }
 }
